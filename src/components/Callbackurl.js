b472486c1f4e7fc4dd25a8f901af8c2f7ae3a28e
mike akoolo <mikeakoolo254@yahoo.com>
2024-03-10 01:02:07 +0300

    initial commit 2

--- src/components/Callbackurl.js
+++ src/components/Callbackurl.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 function Callbackurl() {
+ 
+
+
+
+
+  useEffect(() => {
+    // Make a request to the backend (replace with your actual backend URL)
+    fetch('http://localhost:3001/callback')
+      .then(response => response.json())
+      .then(data => {
+        // Handle the data from the server
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <div>Callbackurl</div>
-  )
-}
+    <div>
+      {/* Your component UI */}
+    </div>
+  );
+};
 
 export default Callbackurl
